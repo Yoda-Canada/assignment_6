9ae8a0798d70245f62a78ccee408e1c8ea823ccb
Your Name <you@example.com>
2020-03-23 19:01:45 -0400

    23

--- data-service.js
+++ data-service.js
@@ -3,14 +3,66 @@ var employees=[];
 var departments=[];
 const Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('dcn3nqsi864ec4', 'ugypnljmcgxhtf', '6559aef653e89b414543cdc950b5501fedd5f95060e6017b0ca0aac96e5542f8', {
-    host: 'ec2-35-174-88-65.compute-1.amazonaws.com',
+var sequelize = new Sequelize('dbp7qkunmq4j95', 'psbyomygbqnqit', '7855ee754c0769bef1663d9edb8b59569840c1b7e8420fef4133d969d8ce4cf4', {
+    host: 'ec2-52-23-14-156.compute-1.amazonaws.com',
     dialect: 'postgres',
     port: 5432,
     dialectOptions: {
     ssl: true
     }
    });
+
+   var Project = sequelize.define('Project', {
+    title: Sequelize.STRING,
+    description: Sequelize.TEXT
+});
+
+sequelize.sync().then(function () {
+
+    // create a new "Project" and add it to the database
+    Project.create({
+        title: 'Project1',
+        description: 'First Project'
+    }).then(function (project) {
+        // you can now access the newly created Project via the variable project
+        console.log("success!")
+    }).catch(function (error) {
+        console.log("something went wrong!");
+    });
+});
+
+const Employee = sequelize.define('Employee', {
+    employeeNum: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    firstName: Sequelize.STRING,
+    lastName: Sequelize.STRING,
+    email: Sequelize.STRING,
+    SSN: Sequelize.STRING,
+    addressStreet: Sequelize.STRING,
+    addressCity: Sequelize.STRING,
+    addressState: Sequelize.STRING,
+    addressPostal: Sequelize.STRING,
+    maritalStatus: Sequelize.STRING,
+    isManager: Sequelize.BOOLEAN,
+    employeeManagerNum: Sequelize.INTEGER,
+    status: Sequelize.STRING,
+    department: Sequelize.INTEGER,
+    hireDate: Sequelize.STRING
+});
+
+var Department = sequelize.define('Department', {
+    departmentId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    departmentName: Sequelize.STRING
+});
+
+
 
 module.exports.getEmployeesByStatus = function (staId) {
   
