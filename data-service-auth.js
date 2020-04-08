fcba5d7ff317b9240882f7baf0e81f1ad0275bef
Your Name <you@example.com>
2020-04-08 11:53:36 -0400

    0408

--- data-service-auth.js
+++ data-service-auth.js
@@ -15,3 +15,16 @@ var userSchema = new Schema({
 });
 
 let User;
+
+module.exports.initialize = function () {
+    return new Promise(function (resolve, reject) {
+        let db = mongoose.createConnection("mongodb+srv://tzhao16:<131131@Ztz>@cluster0-9jngd.mongodb.net/test?retryWrites=true&w=majority");
+        db.on('error', (err)=>{
+            reject(err);
+        });
+        db.once('open', ()=>{
+           User = db.model("Users", userSchema);
+           resolve();
+        });
+    });
+};
